a1b3e8988b53b97755b546ebb2ce8db5dfbddc41
onyoka <72133681+olzhas-ergali@users.noreply.github.com>
2024-12-13 03:05:42 +0500

    Update registration.py

--- services/telegram/handlers/registration.py
+++ services/telegram/handlers/registration.py
@@ -1,153 +1,128 @@
-from aiogram.enums import ParseMode
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, \
-    InlineKeyboardButton, InlineKeyboardMarkup, InlineQuery, InlineQueryResultArticle, InputTextMessageContent
+from aiogram import F, Router
+from aiogram.filters import Command
+from aiogram.fsm.context import FSMContext
+from aiogram.types import Message, CallbackQuery
 from aiogram.utils.i18n import I18n
-from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 from database.database import ORM
-from services.telegram.misc.callbacks import HomeCallback, CitySelect, \
-    AdminCallback, LangCallback, CountrySelect, LangChangeCallBack, RenewSubscription, ChooseModelCallback, \
-    FullButtonCallback
+from services.analyzer.xlsx import get_cities
+from services.telegram.filters.registration import RegistrationFilter
+from services.telegram.misc.callbacks import CitySelect, LangCallback, CountrySelect
+from services.telegram.misc.keyboards import Keyboards
+
+router = Router()
 
 
-class Keyboards:
-    @staticmethod
-    def send_phone(i18n: I18n, user):
-        return ReplyKeyboardMarkup(resize_keyboard=True,
-                                   one_time_keyboard=True,
-                                   keyboard=[[
-                                       KeyboardButton(
-                                           text=i18n.gettext('Поделиться номером телефона', locale=user.lang),
-                                           request_contact=True)
-                                   ]])
+@router.message(F.text, Command("start"))
+async def ask_contact(message: Message,
+                      state: FSMContext,
+                      orm: ORM,
+                      i18n: I18n):
+    user = await orm.user_repo.find_user_by_user_id(message.from_user.id)
+    await state.update_data(columns=user.get_null_columns())
+    msg = await message.answer(text='Для регистрации поделитесь, пожалуйста, номером телефона, нажав на кнопку "Поделится номером телефона" ниже 👇 \n\nНазвание кнопки "Поделится номером телефона"',
+                               reply_markup=Keyboards.send_phone(i18n, user))
+    await state.update_data(msg_id=msg.message_id)
 
-    @staticmethod
-    def home(i18n: I18n, user) -> InlineKeyboardMarkup:
-        keyboard = InlineKeyboardMarkup(inline_keyboard=[
-            [
-                InlineKeyboardButton(text=i18n.gettext("Инструкция", locale=user.lang)+" 📕",
-                                     callback_data=HomeCallback(
-                                         action="instruction").pack())],
-            [
-                InlineKeyboardButton(text=i18n.gettext('Сменить язык', locale=user.lang)+" 🏳️",
-                                     callback_data=LangChangeCallBack(
-                                         action="change", lang="").pack())
-            ],
-            [
-                InlineKeyboardButton(text=i18n.gettext('Наш канал', locale=user.lang)+" 👥",
-                                     url="https://t.me/Yourrepairassistant")
-            ],
-            [
-                InlineKeyboardButton(text=i18n.gettext('Справочник дисков', locale=user.lang)+" 📚",
-                                     switch_inline_query_current_chat="disk ")
-            ]
-        ])
-        if user.role == 'admin':
-            keyboard.inline_keyboard.append([InlineKeyboardButton(text=i18n.gettext("Продлить подписку", locale=user.lang)+" ⏳", switch_inline_query_current_chat="user ")])
 
-        return keyboard
+@router.message(F.contact, RegistrationFilter(filter_column="lang"))
+async def ask_fullname(message: Message, state: FSMContext, orm: ORM, i18n: I18n):
+    user = await orm.user_repo.save_user(message)
+    data = await state.get_data()
+    await message.delete()
+    await message.bot.delete_message(chat_id=message.from_user.id,
+                                     message_id=data["msg_id"])
+    msg = await message.answer(text=i18n.gettext("Выберите язык", user.lang), reply_markup=Keyboards.lang())
+    await state.update_data(msg_id=msg.message_id)
 
-    @staticmethod
-    def back_to_home(i18n: I18n, user) -> InlineKeyboardMarkup:
-        builder = InlineKeyboardBuilder()
-        builder.button(text=i18n.gettext("Назад ◀️", locale=user.lang),
-                       callback_data=HomeCallback(action="back_to_home"))
-        return builder.as_markup()
 
-    @staticmethod
-    def links(links: list, i18n: I18n, user):
-        builder = InlineKeyboardBuilder()
-        for i, link in enumerate(links, start=1):
-            builder.button(text=i18n.gettext("Материал {} 📎", locale=user.lang).format(i), url=link)
-        builder.adjust(1, repeat=True)
-        return builder
+@router.callback_query(LangCallback.filter(), RegistrationFilter(filter_column="fullname"))
+async def ask_fullname(callback: CallbackQuery, callback_data: LangCallback,
+                       state: FSMContext, orm: ORM, i18n: I18n):
+    user = await orm.user_repo.upsert_user(callback.from_user.id, lang=callback_data.lang)
+    # data = await state.get_data()
+    await callback.message.delete()
+    # await message.bot.delete_message(chat_id=message.from_user.id,
+    #                                  message_id=data["msg_id"])
+    msg = await callback.message.answer(text=i18n.gettext("Как вас зовут?", locale=user.lang))
+    await state.update_data(msg_id=msg.message_id)
 
-    @staticmethod
-    def countries(countries: dict):
-        builder = InlineKeyboardBuilder()
-        for i, country in enumerate(countries):
-            builder.button(text=f"{country}",
-                           callback_data=CountrySelect(name=country))
-        builder.adjust(1, repeat=True)
-        return builder.as_markup()
 
-    @staticmethod
-    def cities(countries: list):
-        builder = InlineKeyboardBuilder()
-        for i, country in enumerate(countries):
-            builder.button(text=f"{country}",
-                           callback_data=CitySelect(name=country))
-        # builder.button(text=f"Вперед", callback_data=CitySelect(name=country))
-        builder.adjust(1, repeat=True)
-        return builder.as_markup()
+@router.message(F.text, RegistrationFilter(filter_column="affiliate"))
+async def ask_fullname(message: Message, state: FSMContext, orm: ORM, i18n: I18n):
+    user = await orm.user_repo.upsert_user(message.from_user.id,
+                                           fullname=message.text)
+    data = await state.get_data()
+    await message.delete()
+    await message.bot.delete_message(chat_id=message.from_user.id,
+                                     message_id=data["msg_id"])
+    msg = await message.answer(text=i18n.gettext("Где вы работаете?", locale=user.lang))
+    await state.update_data(msg_id=msg.message_id)
 
-    @staticmethod
-    def empty():
-        return InlineKeyboardMarkup(inline_keyboard=[])
 
-    @staticmethod
-    def guest(user_id, i18n: I18n, user):
-        builder = InlineKeyboardBuilder()
-        builder.button(
-            text=i18n.gettext("Принять ✅", locale=user.lang),
-            callback_data=AdminCallback(action="accept", user_id=user_id))
-        builder.button(
-            text=i18n.gettext("Отклонить ❌", locale=user.lang),
-            callback_data=AdminCallback(action="cancel", user_id=user_id))
-        return builder.as_markup()
-
-    @staticmethod
-    def lang(is_menu=False):
-        builder = InlineKeyboardBuilder()
-        builder.button(
-            text="English 🇺🇸",
-            callback_data=LangCallback(lang="en") if not is_menu else LangChangeCallBack(action='changed', lang="en"))
-        builder.button(
-            text="Русский 🇷🇺",
-            callback_data=LangCallback(lang="ru") if not is_menu else LangChangeCallBack(action='changed', lang="ru"))
-        return builder.as_markup()
-
-    @staticmethod
-    def months(user, i18n: I18n):
-        builder = InlineKeyboardBuilder()
-        builder.button(
-            text=i18n.gettext("1 месяц", locale=user.lang),
-            callback_data=RenewSubscription(user_id=user.user_id, months=1)
+@router.message(F.text, RegistrationFilter(filter_column="country"))
+async def ask_city(message: Message, orm: ORM, state: FSMContext, i18n: I18n):
+    user = await orm.user_repo.upsert_user(message.from_user.id,
+                                           affiliate=message.text)
+    cities = get_cities()
+    data = await state.get_data()
+    try:
+        await message.delete()
+        await message.bot.delete_message(
+            chat_id=message.from_user.id,
+            message_id=data["msg_id"],
         )
-        builder.button(
-            text=i18n.gettext("3 месяца", locale=user.lang),
-            callback_data=RenewSubscription(user_id=user.user_id, months=3)
-        )
-        builder.button(
-            text=i18n.gettext("6 месяцев", locale=user.lang),
-            callback_data=RenewSubscription(user_id=user.user_id, months=6)
-        )
-        builder.button(
-            text=i18n.gettext("1 год", locale=user.lang),
-            callback_data=RenewSubscription(user_id=user.user_id, months=12)
-        )
-        return builder.as_markup()
+    except:
+        pass
+    msg = await message.answer(text=i18n.gettext("Выберите страну", locale=user.lang),
+                               reply_markup=Keyboards.countries(cities))
+    await state.update_data(msg_id=msg.message_id)
 
-    @staticmethod
-    def models(models):
-        builder = InlineKeyboardBuilder()
-        for model in models:
-            builder.row(
-                InlineKeyboardButton(
-                    text=model,
-                    callback_data=ChooseModelCallback(model=model).pack()
-                )
-            )
-        return builder.as_markup()
 
-    @staticmethod
-    def add_full_btn(builder, error_code, model):
-        if error_code.find(':') != -1:
-            error_code = error_code.replace(':', 'doubledott')
-        builder.row(
-            InlineKeyboardButton(
-                text='Полная версия инструкции',
-                callback_data=FullButtonCallback(action='show', error_code=error_code, model=model).pack()
-            )
-        )
-        return builder
+@router.callback_query(CountrySelect.filter(), RegistrationFilter(filter_column="city"))
+async def ask_city(callback: CallbackQuery, callback_data: CountrySelect, orm: ORM, state: FSMContext, i18n: I18n):
+    user = await orm.user_repo.upsert_user(callback.from_user.id, country=callback_data.name)
+    cities = get_cities()
+    msg = await callback.message.edit_text(
+        text=i18n.gettext("Выберите город", locale=user.lang),
+        reply_markup=Keyboards.cities(cities[callback_data.name]))
+    await state.update_data(msg_id=msg.message_id)
+
+
+@router.callback_query(CountrySelect.filter(), RegistrationFilter(filter_column="city"))
+async def ask_city(callback: CallbackQuery, callback_data: CountrySelect, orm: ORM, state: FSMContext, i18n: I18n):
+    user = await orm.user_repo.upsert_user(callback.from_user.id, country=callback_data.name)
+    cities = get_cities()
+    msg = await callback.message.edit_text(
+        text=i18n.gettext("Выберите город", locale=user.lang),
+        reply_markup=Keyboards.cities(cities[callback_data.name]))
+    await state.update_data(msg_id=msg.message_id)
+
+@router.callback_query(CitySelect.filter())
+async def select_city(callback: CallbackQuery,
+                      callback_data: CitySelect,
+                      state: FSMContext,
+                      orm: ORM,
+                      i18n: I18n):
+    await orm.user_repo.upsert_user(
+        callback.from_user.id,
+        city=callback_data.name,
+        role="no_access")
+    user = await orm.user_repo.find_user_by_user_id(callback.from_user.id)
+    data = await state.get_data()
+    text_admins = i18n.gettext("Имя: {}\n"
+                               "Место работы: {}\n"
+                               "Страна: {}\n"
+                               "Город: {}\n"
+                               "Номер: {}").format(user.fullname, user.affiliate, user.country, user.city, user.phone_number)
+    await callback.bot.send_message(
+        orm.settings.application_channel_id,
+        text=text_admins,
+        reply_markup=Keyboards.guest(callback.from_user.id, i18n, user))
+    await callback.bot.delete_message(
+        chat_id=callback.from_user.id,
+        message_id=data["msg_id"],
+    )
+    await callback.message.answer(
+        i18n.gettext("Спасибо за предоставленную информацию!"
+                     f"\nОжидайте пока админ проверит вашу анкету ⌛️", locale=user.lang))
